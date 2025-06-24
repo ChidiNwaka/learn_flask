febc239588fa17a98e6ddc731ec38c930a8f4e3a
Chidi Nwaka <chidi.nwaka@gmail.com>
2025-06-24 06:39:02 -0700

    Updated the readme and ensure that the uuid during a session is consistent

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# learn_flask
+# Learning Flask
+
+I'm following the official [Quick Start]([url](https://flask.palletsprojects.com/en/stable/quickstart/)) documentation by Flask and have added a few additional functions for practice to help me better understand the concepts.
--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 import uuid 
-from flask import Flask
+from flask import Flask, g, session
 from markupsafe import escape
 
-def generate_uuid() -> str:
-    return uuid.uuid4()
-
 app = Flask(__name__)
+app.secret_key = str(uuid.uuid4()) # This generates a random secret key for the session, to be updated with a jwt or similar token
+
+def generate_uuid() -> str:
+    return str(uuid.uuid4())
+
+@app.before_request
+def assign_uuid():
+    print("calling the assign_uuid function first")
+    if 'uuid' not in session:
+        session['uuid'] = generate_uuid()
+    g.uuid = session['uuid']
 
 @app.route("/<name>")
 def hello(name):
@@ -17,8 +25,8 @@ def hello_world() -> str:
 
 @app.route("/admin")
 def get_admin() -> dict:
-    return { "firstName": "Chidi", "uuid": get_uuid(), "lastName": "Nwaka", "role": "Admin", "country": "United States" }
+    return { "firstName": "Chidi", "uuid": g.uuid, "lastName": "Nwaka", "role": "Admin", "country": "United States" }
 
 @app.route("/uuid")
 def get_uuid() -> str:
-    return str(generate_uuid())
+    return g.uuid 
